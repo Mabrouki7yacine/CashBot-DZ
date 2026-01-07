71e7bded2636d3c0ad1a974cdb786bcb227a4a93
MYH <133336507+Mabrouki7yacine@users.noreply.github.com>
2026-01-07 20:13:53 +0100

    rpi4 picamera code

---
+++ Inference_on_RPI/classify_bills.py
@@ -0,0 +1,8 @@
+# this is a helper code used to create a dataset click s to save a sample image
+# run this on your rpi
+
+import cv2
+import os
+import time
+from datetime import datetime
+from picamera2 import Picamera2
